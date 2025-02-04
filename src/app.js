4af6bed8b4d2259291364b1e3cdc4017596a162b
akshay1120 <asjain3031@gmail.com>
2025-02-04 17:51:15 +0530

    database connection

--- src/app.js
+++ src/app.js
@@ -1,14 +1,53 @@
 const express = require('express')
-const app = express()
+const connectDB= require('./config/database')
+const app = express();
+const User = require('./models/user')
 
-app.use('/user',(req,res)=>{
-    res.end("Hello")
+connectDB().then(()=>{
+    console.log("Database connection successfully")
+    app.listen(3000, ()=>{
+        console.log("Server sucessfully listening port bbb")
+    })
+}).catch((err)=>{
+    console.log("Database can not be connected")
+})
+// app.get('/user',(req,res)=>{
+//     res.send({firstname:"saloni",lastname:"pande"})
+// })
+
+app.use(express.json())
+app.post('/signup',async(req,res)=>{
+    
+    console.log(req.body)
+    const user = new User(req.body)
+    //const user = new User(userObj)
+    try{
+    await user.save()
+      res.send("Data sucessfully saved")
+    }catch(err){
+      res.status(400).send("Error Saving the user:" +err.message);
+    }
+   
 })
 
-app.use('/stud',(req,res)=>{
-    res.end("student")
-})
+// app.delete('/user',(req,res)=>{
+//     res.send("Deleted Sucessfully")
+// })
 
-app.listen(3000, ()=>{
-    console.log("Server sucessfully listening port bbb")
-})
+// app.use('/user',(req,res,next)=>{
+//   console.log("Response1");
+//   next();
+ 
+  
+// },(req,res,next)=>{
+//     console.log("2nd Response Call")
+//    // res.end("Response2")
+//    next()
+// },(req,res)=>{
+//     console.log("3rd Response Call")
+//     res.end("Response3")
+// })
+// app.use('/stud',(req,res)=>{
+//     res.end("heloo jnnji")
+// })
+
